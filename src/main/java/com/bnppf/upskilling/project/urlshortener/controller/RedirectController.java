5dccf2b5e196f7db8b0ef43a3fd86886d44750b7
AndyJ75 <andrea.jouannin.devweb@gmail.com>
2020-04-03 16:58:18 +0200

    change on redirection

--- src/main/java/com/bnppf/upskilling/project/urlshortener/controller/RedirectController.java
+++ src/main/java/com/bnppf/upskilling/project/urlshortener/controller/RedirectController.java
@@ -24,28 +24,29 @@ public class RedirectController {
     }
 
     @GetMapping("/{urlKey}")
-    public ResponseEntity<String> getUrlRedirection (@PathVariable String urlKey, @RequestParam(required = false) String urlPass){
+    public ResponseEntity<String> getUrlRedirection(@PathVariable String urlKey) {
+
         Optional<UrlLink> urlLongRetrieved = urlLinkService.getUrlLongFromShortUrl(urlKey);
 
         if (!urlLongRetrieved.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         } else {
-            if (urlLongRetrieved.get().getUrlPassword()!= null &&
-                    (urlLongRetrieved.get().getUrlPassword().equals(urlPass))) {
-                // if wrong password provided do not follow the link and provide error message
-                return ResponseEntity.status(HttpStatus.NON_AUTHORITATIVE_INFORMATION).build();
+            // for UrlLink without password, we redirect directly to UrlLink
+            if (urlLongRetrieved.get().getUrlPassword() == null) {
+                HttpHeaders headers = new HttpHeaders();
+                headers.set("Location", urlLongRetrieved.get().getUrlLong());
+
+                // Update Number of click for given clicked UrlshortLink :
+                Double clickNumberForGivenUrl = urlLongRetrieved.get().getClickNumber() + 1D;
+                urlLongRetrieved.get().setClickNumber(clickNumberForGivenUrl);
+                urlLinkService.updateUrlLink(urlLongRetrieved.get());
+
+                // Redirect to Long url
+                return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).headers(headers).build();
+            } else {
+                // if password required => Open a window to request for Password
+                return ResponseEntity.status(HttpStatus.PROXY_AUTHENTICATION_REQUIRED).build();
             }
-            HttpHeaders headers = new HttpHeaders();
-            headers.set("Location", urlLongRetrieved.get().getUrlLong());
-
-            // Update Number of click for given clicked UrlshortLink :
-            Double clickNumberForGivenUrl = urlLongRetrieved.get().getClickNumber() + 1D;
-            urlLongRetrieved.get().setClickNumber(clickNumberForGivenUrl);
-            urlLinkService.updateUrlLink(urlLongRetrieved.get());
-
-            // Redirect to Long url
-            return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).headers(headers).build();
-
         }
     }
 }
